d950890c73f4f1bb66f7b06de4f0174a497cb043
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-08 11:00:08 +0300

    signup form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
-    imports : [MatButtonModule, MatIconModule],
+    imports : [MatButtonModule, MatIconModule, MatFormFieldModule],
     
-    exports : [MatButtonModule,MatIconModule],
+    exports : [MatButtonModule,MatIconModule, MatFormFieldModule],
 })
 export class MaterialModule {}
