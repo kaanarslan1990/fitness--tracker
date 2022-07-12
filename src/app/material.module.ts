0c7a4f8d3d20bbf95767ada896cb434aee6c41fa
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-12 12:06:32 +0300

    controling layout with @angular/flex-layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports : [MatButtonModule, MatIconModule, MatFormFieldModule],
+    imports : [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ],
     
-    exports : [MatButtonModule,MatIconModule, MatFormFieldModule],
+    exports : [MatButtonModule,MatIconModule, MatFormFieldModule, MatInputModule ],
 })
 export class MaterialModule {}
