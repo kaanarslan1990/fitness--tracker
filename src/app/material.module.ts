47a7dd1b514e191f7316c7b12ccd4fda0a990c2d
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-13 10:44:12 +0300

    adding navigation items

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -18,7 +19,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
+
   ],
 
   exports: [
@@ -30,7 +33,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
 
   ],
 })
