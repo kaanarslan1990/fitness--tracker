77adaca2b20705124e1e09ed49f28c4f5b82a1a2
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-13 16:27:20 +0300

    Injecying&using trainng service

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'khn-new-training',
@@ -7,11 +9,14 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exercises= this.trainingService.getAvailableExercises();
   }
+
 
   onStartTraining() {
     this.trainingStart.emit()
