507a035b123489a2abf702903ed7c2c183bec612
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-13 13:49:15 +0300

    passing data to the dialog

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -5,24 +5,32 @@ import { StopTrainingComponent } from './stop-training.component';
 @Component({
   selector: 'khn-current-training',
   templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.css']
+  styleUrls: ['./current-training.component.css'],
 })
 export class CurrentTrainingComponent implements OnInit {
-  progress= 0;
+  progress = 0;
   timer: number;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
-      if (this.progress >= 100) { 
+      if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    },1000)
+    }, 1000);
   }
-  onStop() { 
+  onStop() {
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent)
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result)
+    });
   }
 }
