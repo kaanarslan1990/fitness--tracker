66a9d556fff7d1eea2751695044a2bc1ac187fb1
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-16 19:46:33 +0300

    restructuring the code

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,41 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {  Subscription } from 'rxjs';
+
 @Component({
   selector: 'khn-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css'],
 })
-export class NewTrainingComponent implements OnInit {
-  public exercises: Observable<Exercise[]>;
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  exercises: Exercise[];
+  exerciseSubscription: Subscription;
 
   constructor(
-    private trainingService: TrainingService,
-    private db: AngularFirestore
+    private trainingService: TrainingService
   ) {}
 
   ngOnInit() {
-    this.exercises = this.db
-      .collection('availableExercises')
-      .snapshotChanges()
-      .pipe(
-        map((docArray) => {
-          return docArray.map((doc) => {
-            return {
-              id: doc.payload.doc.id,
-              ...doc.payload.doc.data() as Exercise
-            };
-          });
-        })
-      )
-      ;
+    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(
+      (exercises) => (this.exercises = exercises)
+    );
+    this.trainingService.fetchAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
+  }
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
+      
   }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,25 +1,41 @@
-import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Subject } from 'rxjs';
 import { Exercise } from './exercise.model';
 
-
+@Injectable()
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
-  private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-  ];
+  exercisesChanged = new Subject<Exercise[]>();
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
   private exercises: Exercise[] = [];
 
-  getAvailableExercises() {
-    return this.availableExercises.slice();
+  constructor(private db: AngularFirestore) {}
+
+  fetchAvailableExercises() {
+    this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            return {
+              id: doc.payload.doc.id,
+              ...(doc.payload.doc.data() as Exercise),
+            };
+          });
+        })
+      )
+      .subscribe((exercises:Exercise[]) => {
+        this.availableExercises = exercises;
+        this.exercisesChanged.next([...this.availableExercises]);
+      }) ;
   }
 
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(
-      ex => ex.id === selectedId
+      (ex) => ex.id === selectedId
     );
     this.exerciseChanged.next({ ...this.runningExercise });
   }
@@ -28,7 +44,7 @@ export class TrainingService {
     this.exercises.push({
       ...this.runningExercise,
       date: new Date(),
-      state: 'completed'
+      state: 'completed',
     });
     this.runningExercise = null;
     this.exerciseChanged.next(null);
@@ -40,7 +56,7 @@ export class TrainingService {
       duration: this.runningExercise.duration * (progress / 100),
       calories: this.runningExercise.calories * (progress / 100),
       date: new Date(),
-      state: 'cancelled'
+      state: 'cancelled',
     });
     this.runningExercise = null;
     this.exerciseChanged.next(null);
@@ -50,7 +66,7 @@ export class TrainingService {
     return { ...this.runningExercise };
   }
 
-  getCompletedOrCancelledExercises() { 
-    return this.exercises.slice()
+  getCompletedOrCancelledExercises() {
+    return this.exercises.slice();
   }
 }
